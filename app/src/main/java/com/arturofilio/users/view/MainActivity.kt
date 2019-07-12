5159f5629efe6da914ad4ba99459b9b8100854db
Arturo Filio Villa <arturfil@hotmail.com>
2019-07-12 11:36:44 -0400

    finally added the implementation of the view on the main activity

--- app/src/main/java/com/arturofilio/users/view/MainActivity.kt
+++ app/src/main/java/com/arturofilio/users/view/MainActivity.kt
@@ -1,13 +1,57 @@
 package com.arturofilio.users.view
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
 import com.arturofilio.users.R
+import com.arturofilio.users.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: ListViewModel
+    private val usersAdapter = UserListAdapter(arrayListOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        usersList.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = usersAdapter
+        }
+
+        swipeRefreshLayout.setOnRefreshListener {
+            swipeRefreshLayout.isRefreshing = false
+            viewModel.refresh()
+        }
+        observerViewModel()
+    }
+
+    fun observerViewModel() {
+        viewModel.users.observe(this, Observer { users ->
+            usersList.visibility = View.VISIBLE
+            users?.let { usersAdapter.updateUsers(it) }
+        })
+
+
+        viewModel.userLoadError.observe(this, Observer { isError ->
+            isError?.let{list_error.visibility = if(it) View.VISIBLE else View.GONE}
+        })
+
+        viewModel.loading.observe(this, Observer { isLoading ->
+            isLoading?.let { loading_view.visibility = if(it) View.VISIBLE else View.GONE
+                if(it) {
+                    list_error.visibility = View.GONE
+                    usersList.visibility = View.GONE
+                }
+            }
+        })
     }
 }
