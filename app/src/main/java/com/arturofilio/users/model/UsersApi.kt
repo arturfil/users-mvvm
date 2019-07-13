fc233f25e5a5b2fed780c847b1abbeed8b3103cb
Arturo Filio Villa <arturfil@hotmail.com>
2019-07-12 20:23:22 -0400

    setting up the UsersApi file for the post request when the user enters the data in the field

--- app/src/main/java/com/arturofilio/users/model/UsersApi.kt
+++ app/src/main/java/com/arturofilio/users/model/UsersApi.kt
@@ -1,6 +1,8 @@
 package com.arturofilio.users.model
 
 import io.reactivex.Single
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.POST
 
@@ -9,7 +11,12 @@ interface UsersApi {
     @GET("/api/users")
     fun getUsers(): Single<Data>
 
+    // This request will send two values as required in the api doc,
+    @FormUrlEncoded
     @POST("/api/users")
-    fun createUser(): Single<User>
+    fun createUser(
+        @Field("name") name: String,
+        @Field("job") job: String
+    ): Single<User>
 
 }
